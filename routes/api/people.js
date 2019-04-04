cdb3fb65a01df14e3c6787c47fb3b97eab12c654
Michael <mikeramos92@gmail.com>
2019-04-03 21:23:05 -0400

    cleanup

--- routes/api/people.js
+++ routes/api/people.js
@@ -9,12 +9,8 @@ const key = 'history';
 bluebird.promisifyAll(redis.RedisClient.prototype);
 
 router.get('/history', async(req, res) => {
-    /*
-    This route will respond with an array of the last 20 users in the cache from the recently viewed list. You can have duplicate users in your 20 user list.
-    For each user being printed out, you will print out their entire user information.
-     */
     try {
-        const history = await client.lrangeAsync(key, 0, 20);
+        const history = await client.lrangeAsync(key, 0, 19);
         let users = [];
         for (let user in history) {
             users.push(JSON.parse(history[user]));
@@ -57,12 +53,6 @@ router.get('/:id', async(req, res, next) => {
 
 // if not in cache
 router.get('/:id', async(req, res) => {
-    /*
-    This route will do two things:
-    1) Check if the user has a cache entry in redis. If so, render the result from that cache entry
-    2) If not, query the data module for the person and fail the request if they are not found, or send JSON and cache the result if they are found.
-    If the person is found, then you will add their ID to a list of recently viewed people (this list allows duplicates!) ordered by most recent accessor first. This list can be infinitely large.
-     */
     const reqUser = await userdata.getById(req.params.id);
 
     if (reqUser) {
@@ -76,10 +66,6 @@ router.get('/:id', async(req, res) => {
         res.send({'message': `User with ID ${req.params.id} does not exist`})
     }
 });
-
-// router.get('/list/all', async(req, res) => {
-//     res.send(userdata.userList);
-// });
 
 module.exports = router;
 
