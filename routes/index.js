3cb8309e41e7ec4112239e9f6eacbe7704255252
Michael <mikeramos92@gmail.com>
2019-04-03 21:21:20 -0400

    update urls

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const peopleRoutes = require('./api/people');
 
 const constructorMethod = app => {
-    app.use('/people', peopleRoutes);
+    app.use('/api/people', peopleRoutes);
     app.use('*', (req, res) => {
         res.sendStatus(200);
     })
